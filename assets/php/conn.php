3ac70804b1214a6031de63509b2ca2329aaa67b0
Yasteel <64553959+Yasteel@users.noreply.github.com>
2022-02-07 01:45:43 +0200

    updated

--- assets/php/conn.php
+++ assets/php/conn.php
@@ -1,13 +1,13 @@
 <?php
 
-// $serverName = "41.78.244.45";
-// $serverUserName = "tr_yasteel";
-// $serverPass = "%NUJ4}Z[+}Ga";
-// $dbName = "tr_yasteel";
+$serverName = "41.78.244.45";
+$serverUserName = "tr_yasteel";
+$serverPass = "%NUJ4}Z[+}Ga";
+$dbName = "tr_yasteel";
 
-$serverName = "localhost";
-$serverUserName = "root";
-$serverPass = "";
-$dbName = "pos";
+// $serverName = "localhost";
+// $serverUserName = "root";
+// $serverPass = "";
+// $dbName = "pos";
 
 $conn = mysqli_connect($serverName,$serverUserName,$serverPass,$dbName);
